02c479955ea438608ef83dda3c8c44b0fa2bd964
Rajeshaalam <52394096+Rajeshaalam@users.noreply.github.com>
2020-08-28 18:06:55 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-"""myproject URL Configuration
+"""project2 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapp import views
+from app2 import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/',views.hi,name='hai')
+    path('sample/',views.sample,name='sample'),
 ]
