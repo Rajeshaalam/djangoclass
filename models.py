ae7a98eff2df6209549726a51bb14dcbf3466a43
Rajeshaalam <52394096+Rajeshaalam@users.noreply.github.com>
2020-09-01 19:42:27 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class student(models.Model):
+	First_Name = models.CharField(max_length=30)
+	Last_Name = models.CharField(max_length=30)
+	Branch = models.CharField(max_length=10)
+	Age = models.IntegerField()
+	Email = models.EmailField()
+
+
+		
